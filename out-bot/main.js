b19a81676068c695e69bf0bb30e2d92fdbd9817c
drahunpavel <drahunpavel@gmail.com>
2021-04-14 23:15:44 +0300

    upd

--- out-bot/main.js
+++ out-bot/main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GW out bot
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.1.1
 // @description  try to take over the world!
 // @author       You
 // @updateURL    https://raw.githubusercontent.com/drahunpavel/GW/main/out-bot/main.js
