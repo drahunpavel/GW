b0401b49cd8853089f432bc6e42043a89f987025
drahunpavel <drahunpavel@gmail.com>
2021-04-14 23:10:12 +0300

    upd

--- out-bot/main.js
+++ out-bot/main.js
@@ -4,6 +4,8 @@
 // @version      1.1
 // @description  try to take over the world!
 // @author       You
+// @updateURL    https://raw.githubusercontent.com/drahunpavel/GW/main/out-bot/main.js
+// @downloadURL  https://raw.githubusercontent.com/drahunpavel/GW/main/out-bot/main.js
 // @match        https://www.gwars.ru/walk.op.php*
 // @grant        none
 // ==/UserScript==
