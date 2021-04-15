b3e1ba4af5c53c7d819952ef5a11deb22f1ce787
drahunpavel <drahunpavel@gmail.com>
2021-04-16 02:16:40 +0300

    @version      1.1.11

--- out-bot/main.js
+++ out-bot/main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GW out bot
 // @namespace    https://github.com/drahunpavel/GW/tree/main/out-bot
-// @version      1.1.10
+// @version      1.1.11
 // @description  try to take over the world!
 // @author       You
 // @updateURL    https://raw.githubusercontent.com/drahunpavel/GW/main/out-bot/main.js
