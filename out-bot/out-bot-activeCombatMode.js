989d7588387b90844a706fbfff31e16305e84101
drahunpavel <drahunpavel@gmail.com>
2021-04-16 03:29:26 +0300

    activeCombatMode version      1.1.1

--- out-bot/out-bot-activeCombatMode.js
+++ out-bot/out-bot-activeCombatMode.js
@@ -8,8 +8,47 @@
 // @grant        none
 // ==/UserScript==
 
+/*
+---1.1.1
+реализованы автоходы, без навыков
+---
+*/
+
 (function () {
     'use strict';
 
-    console.log('--activeCombatMode')
+    const minTimer = 2000;
+    const maxTimer = 3000;
+
+    const grenade = document.getElementById("bagaboom"); //чекбокс "бросить гранату"
+    const comeUp = document.getElementById("walk"); //чекбокс "подоходить всегда"
+    const selectedOppenent = document.getElementById('euids'); //выбор противника
+
+    if (selectedOppenent) {
+        const generalActiveSkill = document.getElementById("apmid");
+        const specialActiveSkill = document.getElementById("apsid");
+        const greenbuttonTags = document.getElementsByClassName('greenbutton'); //все кнопки нпса
+
+        const tbody = document.querySelectorAll('table > tbody');
+        const battleModeComponents = tbody[4].querySelectorAll('tr');
+        const battleMode = battleModeComponents[1];
+        const teamLeft = battleMode.children[0]; //команда игрока
+        const battleСontrol = battleMode.children[1]; //панель управления
+
+        const sendMoveButton = battleСontrol.getElementsByTagName('table') && battleСontrol.getElementsByClassName('wb') && battleСontrol.getElementsByTagName('a');
+
+        //если есть кнопка сделать ход, кликаю по ней
+        if (sendMoveButton.length) {
+            window.setTimeout(function () {
+                sendMoveButton[0].click();
+            }, randomInteger(minTimer, maxTimer));
+        };
+
+        //гереация рандомного числа для отработки таймера ходов
+        function randomInteger(min, max) {
+            // получить случайное число от (min-0.5) до (max+0.5)
+            let rand = min - 0.5 + Math.random() * (max - min + 1);
+            return Math.round(rand);
+        };
+    };
 })();
