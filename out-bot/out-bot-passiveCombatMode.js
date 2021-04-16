fc7de9891cd490fa5a500bd0a99606ec612479a8
drahunpavel <drahunpavel@gmail.com>
2021-04-16 03:35:55 +0300

    passiveCombatMode version      1.1.1

--- out-bot/out-bot-passiveCombatMode.js
+++ out-bot/out-bot-passiveCombatMode.js
@@ -11,5 +11,21 @@
 (function () {
     'use strict';
 
-    console.log('--passiveCombatMode')
+    const minTimer = 2000;
+    const maxTimer = 3000;
+
+    const selectedOppenent = document.getElementById('euids'); //выбор противника
+
+    if (!selectedOppenent) {
+        window.setTimeout(function () {
+            document.location.reload();
+        }, randomInteger(minTimer, maxTimer));
+    };
+
+    //гереация рандомного числа для отработки таймера ходов
+    function randomInteger(min, max) {
+        // получить случайное число от (min-0.5) до (max+0.5)
+        let rand = min - 0.5 + Math.random() * (max - min + 1);
+        return Math.round(rand);
+    };
 })();
