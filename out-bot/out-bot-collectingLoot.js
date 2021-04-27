958489d418c47ce5fa29a41fa547353a27884c4a
drahunpavel <drahunpavel@gmail.com>
2021-04-27 07:02:55 +0300

    out-bot-collectingLoot init

---
+++ out-bot/out-bot-collectingLoot.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         GW out-bot-collectingLoot
+// @namespace    https://github.com/drahunpavel/GW/tree/main/out-bot
+// @version      1.1.1
+// @description  try to take over the world!
+// @author       https://github.com/drahunpavel
+// @updateURL    https://raw.githubusercontent.com/drahunpavel/GW/main/out-bot/out-bot-notifications.js
+// @downloadURL  https://raw.githubusercontent.com/drahunpavel/GW/main/out-bot/out-bot-notifications.js
+// @match        https://www.gwars.ru/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+})();
