9b8955e1b475968c5baca3777b6636b6aaef3d9e
drahunpavel <drahunpavel@gmail.com>
2021-04-27 07:34:52 +0300

    upd

--- out-bot/out-bot-collectingLoot.js
+++ out-bot/out-bot-collectingLoot.js
@@ -1,4 +1,4 @@
-function pickUpLoot() {
+export default function pickUpLoot() {
     const takeButton = document.getElementById("takebutt"); //кнопка "Взять"
     const lootDescription = document.getElementsByClassName('wb')[5];
     const loot = lootDescription.querySelectorAll('td > b')[1].innerText;
