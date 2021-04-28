46dc64598f86858fc7d64d66b830e3897accdc91
drahunpavel <drahunpavel@gmail.com>
2021-04-28 09:05:28 +0300

    гзв

--- out-bot/out-bot-passiveCombatMode.js
+++ out-bot/out-bot-passiveCombatMode.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         GW out-bot-passiveCombatMode
 // @namespace    https://github.com/drahunpavel/GW/tree/main/out-bot
-// @version      1.1.4
+// @version      1.1.5
 // @description  try to take over the world!
 // @author       https://github.com/drahunpavel
-// @updateURL    https://raw.githubusercontent.com/drahunpavel/GW/main/out-bot/out-bot-fightResult.js
-// @downloadURL  https://raw.githubusercontent.com/drahunpavel/GW/main/out-bot/out-bot-fightResult.js
+// @updateURL    https://raw.githubusercontent.com/drahunpavel/GW/main/out-bot/out-bot-passiveCombatMode.js
+// @downloadURL  https://raw.githubusercontent.com/drahunpavel/GW/main/out-bot/out-bot-passiveCombatMode.js
 // @match        https://www.gwars.ru/b0*
 // @grant        none
 // ==/UserScript==
